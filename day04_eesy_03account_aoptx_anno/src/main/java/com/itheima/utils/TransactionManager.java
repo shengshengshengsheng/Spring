338275f47cb15c59ae6df6ff44a2900d4fdf5503
xuqiangsheng <1591330057@qq.com>
2020-07-28 13:15:32 +0800

    update

--- day04_eesy_03account_aoptx_anno/src/main/java/com/itheima/utils/TransactionManager.java
+++ day04_eesy_03account_aoptx_anno/src/main/java/com/itheima/utils/TransactionManager.java
@@ -1,5 +1,6 @@
 package com.itheima.utils;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -22,7 +23,7 @@ public class TransactionManager {
     /**
      * 开启事务
      */
-    @Before("pt1()")
+//    @Before("pt1()")
     public void beginTransaction(){
         try {
             connectionUtils.getThreadConnection().setAutoCommit(false);
@@ -33,7 +34,7 @@ public class TransactionManager {
     /**
      * 提交事务
      */
-    @AfterReturning("pt1()")
+//    @AfterReturning("pt1()")
     public void commit(){
         try {
             connectionUtils.getThreadConnection().commit();
@@ -44,7 +45,7 @@ public class TransactionManager {
     /**
      * 回滚事务
      */
-    @AfterThrowing("pt1()")
+//    @AfterThrowing("pt1()")
     public void rollback(){
         try {
             connectionUtils.getThreadConnection().rollback();
@@ -55,7 +56,7 @@ public class TransactionManager {
     /**
      * 释放连接
      */
-    @After("pt1()")
+//    @After("pt1()")
     public void release(){
         try {
             //还回连接池
@@ -64,6 +65,29 @@ public class TransactionManager {
             connectionUtils.removeConnection();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        }
+    }
+
+    @Around("pt1()")
+    public Object aroundAdvice(ProceedingJoinPoint pjp){
+        Object returnValue = null;
+        try {
+            //1.获取参数
+            Object[] args= pjp.getArgs();
+            //2.开启事务
+            this.beginTransaction();
+            //3.执行方法
+            returnValue = pjp.proceed(args);
+            //4.提交事务
+            this.commit();
+            return returnValue;
+        }catch (Throwable throwable){
+            //5.回滚事务
+            this.rollback();
+            throw new RuntimeException(throwable);
+        }finally {
+            //6.释放资源
+            this.release();
         }
     }
 }
